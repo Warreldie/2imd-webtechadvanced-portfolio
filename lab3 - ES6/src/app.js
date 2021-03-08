922fe97c701182a4efa5710cde9e5ee1d0a99173
Ward Vanelverdinghe <vanelverdinghe.ward@gmail.com>
2021-03-08 21:10:41 +0100

    Came to add but the add is undefined

--- lab3 - ES6/src/app.js
+++ lab3 - ES6/src/app.js
@@ -1,84 +1,61 @@
 class Note {
-    constructor(title) {
-      this.title = title;
-      this.element = this.createElement(title);
-    }
-  
-    createElement(title) {
-      let newNote = document.createElement("li");
-  
-      // HINT🤩 newNote.addEventListener('click', this.remove.bind(newNote));
-  
-      return newNote;
-    }
-  
-    add() {
-      // HINT🤩
-      // this function should append the note to the screen somehow
-      console.log("Went into add");
-      var ul = document.getElementById("taskList");
-      var li = document.createElement("li");
-      li.appendChild(document.createTextNode(text));
-      ul.appendChild(li);
-    
-    }
-  
-    saveToStorage() {
-      // HINT🤩
-      // localStorage only supports strings, not arrays
-      // if you want to store arrays, look at JSON.parse and JSON.stringify
-    }
-  
-    remove() {
-      // HINT🤩 the meaning of 'this' was set by bind() in the createElement function
-      // in this function, 'this' will refer to the current note element
-      // .removeChild(this)
-      // remove the item from screen and from localstorage
-    }
+  constructor(title) {
+    this.title = title;
+    this.element = this.createElement(title);
   }
-  
-  class App {
-    constructor() {
-      // pressing the enter key in the text field triggers the createNote function
-      let taskinput = document.getElementById("taskInput");
-      taskinput.addEventListener("keyup", (e) => {
-        if(e.key === "Shift"){
-          //Voorlopig met Shift
-          app.createNote();
-        }
-      });
-  
-      // HINT🤩
-      // this.txtTodo = ???
-      // this.txtTodo.addEventListener("keypress", this.createNote.bind(this));
-      // read up on .bind() -> we need to pass the current meaning of this to the eventListener
-      // when the app loads, we can show previously saved noted from localstorage
-      // this.loadNotesFromStorage();
-    }
-  
-    loadNotesFromStorage() {
-      // HINT🤩
-      // load all notes from storage here and add them to the screen
-    }
-  
-    createNote(e) {
-      //Text te pakken hebben
-      let note = document.getElementById("taskInput").value;
-      console.log(note); 
+  createElement(title) {
+    let newNote = document.createElement("li");
 
+    // HINT🤩 newNote.addEventListener('click', this.remove.bind(newNote));
 
-      // this function should create a new note by using the Note() class
-      // HINT🤩
-      note.add();
-      // note.saveToStorage();
-      // clear the text field with .reset in this class
-      // if (e.key === "Enter")
-    }
-  
-    reset() {
-      // this function should reset the form / clear the text field
-    }
+    return newNote;
   }
-  
-  let app = new App();
-  
+  add() {
+    var ul = document.getElementById("taskList");
+    this.element.append(this.txtTodo);
+    ul.appendChild(this.element);
+    console.log(this);
+    // HINT🤩
+    // this function should append the note to the screen somehow
+  }
+  saveToStorage() {
+    console.log("Got into saveStorage!")
+    // HINT🤩
+    // localStorage only supports strings, not arrays
+    // if you want to store arrays, look at JSON.parse and JSON.stringify
+  }
+}
+
+class App {
+  constructor() {
+    // Get the input field
+    this.txtTodo = document.getElementById("taskInput");
+    // Execute a function when the user presses a key on the keyboard
+    this.txtTodo.addEventListener("keypress", this.createNote.bind(this));
+
+    // HINT🤩
+    // when the app loads, we can show previously saved noted from localstorage
+    // this.loadNotesFromStorage();
+  }
+  createNote(e) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (e.keyCode === 13) {
+      //console.log(this.txtTodo.value);
+      this.txtTodo = new Note(this.txtTodo.value);
+      
+      this.txtTodo.add();
+      this.txtTodo.saveToStorage();
+
+      // Cancel the default action, if needed
+      e.preventDefault();
+    }
+    // this function should create a new note by using the Note() class
+    // HINT🤩
+    // note.add();
+    // note.saveToStorage();
+    // clear the text field with .reset in this class
+    // if (e.key === "Enter")
+  }
+}
+
+let app = new App();
