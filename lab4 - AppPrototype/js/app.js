12ee8a4a0a84548681abb90ed767c40f4f1fadc1
Ward Vanelverdinghe <vanelverdinghe.ward@gmail.com>
2021-03-10 19:00:08 +0100

    Made the first API work with concert locations

--- lab4 - AppPrototype/js/app.js
+++ lab4 - AppPrototype/js/app.js
@@ -1,7 +1,37 @@
-class App{
-    constructor(){
-        console.log("Constructor");
-    }
+class App {
+  constructor() {
+    this.getLocation();
+    this.lat;
+    this.lng;
+  }
+  getLocation() {
+    navigator.geolocation.getCurrentPosition(
+      this.gotLocation.bind(this),
+      this.errorLocation.bind(this)
+    );
+  }
+  gotLocation(result) {
+    this.lat = result.coords.latitude;
+    this.lng = result.coords.longitude;
+    this.getPokémon();
+  }
+  getPokémon() {
+    //https://app.ticketmaster.com/discovery/v2/events.json?apikey={apikey}
+    let url =`https://app.ticketmaster.com/discovery/v2/events.json?latlong=${this.lat},${this.lng}&apikey=Zg1wlEt1st0tvjr6U8XvADgBR45m59Fh`;
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .then((err) => {
+        console.log(err);
+      });
+  }
+  errorLocation(err) {
+    console.log(err);
+  }
 }
 
 let app = new App();
