7b53cb8b47d99a435858e92fe38171257e75e557
Ward Vanelverdinghe <vanelverdinghe.ward@gmail.com>
2021-03-12 11:05:47 +0100

    Made ToDo app work

--- lab3 - ES6/src/app.js
+++ lab3 - ES6/src/app.js
@@ -5,8 +5,8 @@ class Note {
   }
   createElement(title) {
     let newNote = document.createElement("li");
-    newNote.innerHTML = title; 
-    // HINT🤩 newNote.addEventListener('click', this.remove.bind(newNote));
+    newNote.innerHTML = title;
+    newNote.addEventListener('click', this.remove.bind(newNote));
 
     return newNote;
   }
@@ -15,11 +15,22 @@ class Note {
     document.getElementById("taskList").appendChild(this.element);
   }
   saveToStorage() {
-    // if you want to store arrays, look at JSON.parse and JSON.stringify
-    // localStorage only supports strings, not arrays
     let notes = [];
+    // if you want to store arrays, look at JSON.parse and JSON.stringify
     notes = JSON.parse(localStorage.getItem("notes"));
     notes.push(this.title);
+    // localStorage only supports strings, not arrays
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }
+  remove() {
+    // remove the item from screen and from localstorage
+    document.getElementById("taskList").removeChild(this);
+    
+    let notes = localStorage.getItem("notes");
+    notes = JSON.parse(notes);
+    let text = this.innerHTML;
+    let index = notes.indexOf(text);
+    notes.splice(index, 1);
     localStorage.setItem("notes", JSON.stringify(notes));
   }
 }
@@ -31,46 +42,39 @@ class App {
     // Execute a function when the user presses a key on the keyboard
     this.txtTodo.addEventListener("keypress", this.createNote.bind(this));
 
-    // HINT🤩
     // when the app loads, we can show previously saved noted from localstorage
     this.loadNotesFromStorage();
   }
   loadNotesFromStorage() {
-    let notes = localStorage.getItem(notes);
-    
-    for(let i = 0; i < no.length; i++){
-      console.log(notes[i]);
-    }
-  
-    // HINT🤩
     // load all notes from storage here and add them to the screen
+    let notes = localStorage.getItem("notes");
+
+    notes = JSON.parse(notes);
+
+    notes.forEach((title) => {
+      let newNote = new Note(title);
+      newNote.add();
+    });
   }
   createNote(e) {
     // Number 13 is the "Enter" key on the keyboard
     if (e.keyCode === 13) {
+      // this function should create a new note by using the Note() class
       let note = new Note(this.txtTodo.value);
 
       note.add();
       note.saveToStorage();
+      // clear the text field with .reset in this class
+      this.reset();
 
       // Cancel the default action, if needed
       e.preventDefault();
     }
-    // this function should create a new note by using the Note() class
-    // HINT🤩
-    // note.add();
-    // note.saveToStorage();
-    // clear the text field with .reset in this class
-    // if (e.key === "Enter")
+  }
+  reset() {
+    // this function should reset the form / clear the text field
+    this.txtTodo.value = "";
   }
 }
 
 let app = new App();
-
-/*
-createElement(title) { 
-  let newNote = document.createElement("li"); 
-  newNote.innerHTML = title; 
-  newNote.addEventListener('click', this.remove.bind(newNote)); 
-  return newNote; }
-*/
