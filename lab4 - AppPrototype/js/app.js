10c94ddd1dc1a02f40f50f6e33678e03847b90e6
Ward Vanelverdinghe <vanelverdinghe.ward@gmail.com>
2021-03-16 21:28:48 +0100

    Made API for concert work better and started with transport API

--- lab4 - AppPrototype/js/app.js
+++ lab4 - AppPrototype/js/app.js
@@ -13,11 +13,33 @@ class App {
   gotLocation(result) {
     this.lat = result.coords.latitude;
     this.lng = result.coords.longitude;
-    this.getPokémon();
+    this.getConcert();
+    this.getTrain();
   }
-  getPokémon() {
+  getConcert() {
     //https://app.ticketmaster.com/discovery/v2/events.json?apikey={apikey}
     let url =`https://app.ticketmaster.com/discovery/v2/events.json?latlong=${this.lat},${this.lng}&apikey=Zg1wlEt1st0tvjr6U8XvADgBR45m59Fh`;
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        document.querySelector("#title").innerHTML = data._embedded.events[0]["name"];
+        document.querySelector("#image").src = data._embedded.events[0]["images"][0]["url"];
+        document.querySelector("#location").innerHTML = data._embedded.events[0]["_embedded"]["venues"][0]["name"];
+        document.querySelector("#location2").innerHTML = data._embedded.events[0]["_embedded"]["venues"][0]["city"]["name"];
+        document.querySelector("#distance").innerHTML = data._embedded.events[0]["_embedded"]["venues"][0]["distance"];
+        document.querySelector("#date").innerHTML = data._embedded.events[0]["dates"]["start"]["localDate"];
+        document.querySelector("#time").innerHTML = data._embedded.events[0]["dates"]["start"]["localTime"];
+
+        //let city = data._embedded.events[0]["_embedded"]["venues"][0]["city"]["name"];
+      })
+      .then((err) => {
+        console.log(err);
+      });
+  }
+  getTrain() {
+    let url =`https://api.irail.be/connections/?from=Gent-Sint-Pieters&to=Mechelen&date=170321&time=1230&timesel=departure&format=json&lang=en&typeOfTransport=automatic&alerts=false&results=6`;
     fetch(url)
       .then((response) => {
         return response.json();
